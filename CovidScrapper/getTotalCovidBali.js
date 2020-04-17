23456eaf4000e4b711d24d0212478d2450b0928f
Satya Wikananda <satyawikananda456@gmail.com>
2020-04-17 13:10:43 +0800

    :zap: Move kabupaten scrap

--- CovidScrapper/getTotalCovidBali.js
+++ CovidScrapper/getTotalCovidBali.js
@@ -17,16 +17,59 @@ const getTotalCovidBali = async () => {
        .then(html => {
            const $ = cheerio.load(html)
            let totalData = []
+           let kab = []
+           let positif = []
+           let perawatan = []
+           let sembuh = []
+           let meninggal = []
 
            $('body > div > div > div > div > div > div > div > div.card-body > h3').each((i,e) => {
                totalData.push($(e).text().trim())
            })
+
+           // Data list kabupaten
+            $('body > div > div > div > div > div:nth-child(3) > div.col-md-12.col-md-offset-1 > div:nth-child(1) > div.card-body > div > table > tbody > tr > td:nth-child(1)').each((i,e) => {
+                kab.push($(e).text().trim())
+            })
+
+            // Data jumlah positif
+            $('body > div > div > div > div > div:nth-child(3) > div.col-md-12.col-md-offset-1 > div:nth-child(1) > div.card-body > div > table > tbody > tr > td:nth-child(2)').each((i,e) => {
+                positif.push($(e).text().trim())
+            })
+            
+            // Data jumlah perawatan
+            $('body > div > div > div > div > div:nth-child(3) > div.col-md-12.col-md-offset-1 > div:nth-child(1) > div.card-body > div > table > tbody > tr > td:nth-child(3)').each((i,e) => {
+                perawatan.push($(e).text().trim())
+            })
+
+            // Data Jumlah sembuh
+            $('body > div > div > div > div > div:nth-child(3) > div.col-md-12.col-md-offset-1 > div:nth-child(1) > div.card-body > div > table > tbody > tr > td:nth-child(4)').each((i,e) => {
+                sembuh.push($(e).text().trim())
+            })
+
+            // Data jumlah meninggal
+            $('body > div > div > div > div > div:nth-child(3) > div.col-md-12.col-md-offset-1 > div:nth-child(1) > div.card-body > div > table > tbody > tr > td:nth-child(5)').each((i,e) => {
+                meninggal.push($(e).text().trim())
+            })
+
            data = {}
            data.provinsi = "bali"
            data.positif = totalData[0].replace(' Org','')
            data.perawatan = totalData[1].replace(' Org','')
            data.sembuh = totalData[2].replace(' Org','')
            data.meninggal = totalData[3].replace(' Org','')
+           data.kabupaten = []
+
+           let i
+           for(i = 0; i < kab.length; i++){
+                data.kabupaten[i] = {
+                    kabupaten: kab[i],
+                    positif: positif[i],
+                    perawatan: perawatan[i],
+                    sembuh: sembuh[i],
+                    meninggal: meninggal[i]
+                }
+            }
 
            return data
        })
