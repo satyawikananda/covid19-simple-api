11a2dedd6fafd6671f0311d54f6e20a661edfa94
Satya Wikananda <satyawikananda456@gmail.com>
2020-04-20 22:56:58 +0800

    :zap: Update data scrap

--- utils/getTotalCovidBali.js
+++ utils/getTotalCovidBali.js
@@ -14,10 +14,14 @@ const getTotalCovidBali = async () => {
         const $ = cheerio.load(html.data);
         let totalData = [];
         let kab = [];
-        let positif = [];
-        let perawatan = [];
-        let sembuh = [];
-        let meninggal = [];
+        let ppln = [];
+        let ppdn = [];
+        let transmisiLokal = [];
+        let positifLainnya = [];
+        let totalPositif = [];
+        let dalamPerawatan = [];
+        let sembuhKab = [];
+        let meninggalKab = [];
 
         $(
           "body > div > div > div > div > div > div > div > div.card-body > h3"
@@ -36,28 +40,52 @@ const getTotalCovidBali = async () => {
         $(
           "body > div > div > div > div > div:nth-child(3) > div.col-md-12.col-md-offset-1 > div:nth-child(1) > div.card-body > div > table > tbody > tr > td:nth-child(2)"
         ).each((i, e) => {
-          positif.push($(e).text().trim());
+          ppln.push($(e).text().trim());
         });
 
         // Data jumlah perawatan
         $(
           "body > div > div > div > div > div:nth-child(3) > div.col-md-12.col-md-offset-1 > div:nth-child(1) > div.card-body > div > table > tbody > tr > td:nth-child(3)"
         ).each((i, e) => {
-          perawatan.push($(e).text().trim());
+          ppdn.push($(e).text().trim());
         });
 
         // Data Jumlah sembuh
         $(
           "body > div > div > div > div > div:nth-child(3) > div.col-md-12.col-md-offset-1 > div:nth-child(1) > div.card-body > div > table > tbody > tr > td:nth-child(4)"
         ).each((i, e) => {
-          sembuh.push($(e).text().trim());
+          transmisiLokal.push($(e).text().trim());
         });
 
         // Data jumlah meninggal
         $(
           "body > div > div > div > div > div:nth-child(3) > div.col-md-12.col-md-offset-1 > div:nth-child(1) > div.card-body > div > table > tbody > tr > td:nth-child(5)"
         ).each((i, e) => {
-          meninggal.push($(e).text().trim());
+          positifLainnya.push($(e).text().trim());
+        });
+
+        $(
+          "body > div > div > div > div > div:nth-child(3) > div.col-md-12.col-md-offset-1 > div:nth-child(1) > div.card-body > div > table > tbody > tr > td:nth-child(6)"
+        ).each((i, e) => {
+          totalPositif.push($(e).text().trim());
+        });
+
+        $(
+          "body > div > div > div > div > div:nth-child(3) > div.col-md-12.col-md-offset-1 > div:nth-child(1) > div.card-body > div > table > tbody > tr > td:nth-child(7)"
+        ).each((i, e) => {
+          dalamPerawatan.push($(e).text().trim());
+        });
+
+        $(
+          "body > div > div > div > div > div:nth-child(3) > div.col-md-12.col-md-offset-1 > div:nth-child(1) > div.card-body > div > table > tbody > tr > td:nth-child(8)"
+        ).each((i, e) => {
+          sembuhKab.push($(e).text().trim());
+        });
+
+        $(
+          "body > div > div > div > div > div:nth-child(3) > div.col-md-12.col-md-offset-1 > div:nth-child(1) > div.card-body > div > table > tbody > tr > td:nth-child(9)"
+        ).each((i, e) => {
+          meninggalKab.push($(e).text().trim());
         });
 
         data = {};
@@ -72,10 +100,16 @@ const getTotalCovidBali = async () => {
         for (i = 0; i < kab.length; i++) {
           data.kabupaten[i] = {
             kabupaten: kab[i],
-            positif: parseInt(positif[i]),
-            perawatan: parseInt(perawatan[i]),
-            sembuh: parseInt(sembuh[i]),
-            meninggal: parseInt(meninggal[i]),
+            positif: {
+              ppln: parseInt(ppln[i]),
+              ppdn: parseInt(ppdn[i]),
+              transmisiLokal: parseInt(transmisiLokal[i]),
+              positifLainnya: parseInt(positifLainnya[i]),
+              totalPositif: parseInt(totalPositif[i]),
+            },
+            perawatan: parseInt(dalamPerawatan[i]),
+            sembuh: parseInt(sembuhKab[i]),
+            meninggal: parseInt(meninggalKab[i]),
           };
         }
         resolve(data);
